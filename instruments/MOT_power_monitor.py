69224d8d5a6cb5cf25cde5ae002379ed4677342d
yimingzhangJPG <yimingzhang.zym@gmail.com>
2022-09-09 15:01:50 -0400

    Monitor MOT powers on picoscope

---
+++ instruments/MOT_power_monitor.py
@@ -0,0 +1,52 @@
+import numpy as np
+from ps4824a_wrapper_blockmode_utils import Picoscope
+from status_monitor import StatusMonitor
+import datetime
+import time
+
+class MOTPowerMonitor(StatusMonitor):
+    def __init__(self, refresh_time = 5, **kwargs):
+        super().__init__(self, **kwargs)
+        self.scope = Picoscope(0, serial='IW968/0159', verbose=True)
+        #TODO: import scope   instead of hardcoding here
+        self.channel_dict = {
+            'A': {'channel_label': 'Trigger','channel_range_mv': 2000},            
+            'B': {'channel_label': 'NaMOT_Top','channel_range_mv': 1000},
+            'C': {'channel_label': 'NaMOT_Bottom','channel_range_mv': 2000},
+            'D': {'channel_label': 'NaMOT_KSlowerWindow','channel_range_mv': 500},
+            'E': {'channel_label': 'NaMOT_KSlower','channel_range_mv': 2000},
+            'F': {'channel_label': 'NaMOT_NaSlowerWindow','channel_range_mv': 5000},
+            'G': {'channel_label': 'NaMOT_NaSlower','channel_range_mv': 5000},                                                            
+        }
+        self._configure_picoscope(self.scope)
+        self.refresh_time = refresh_time
+    
+    def _configure_picoscope(self, scope):
+        for chl_idx, params in self.channel_dict.items():
+            scope.setup_channel(chl_idx, channel_range_mv=params['channel_range_mv'])
+        scope.setup_trigger('A', trigger_threshold_mv=1500)
+        scope.setup_block(block_size=100000, block_duration=1, pre_trigger_percent=0)
+
+    def main(self):
+        with self.scope as scope:
+            scope = self.scope
+            while True:
+                scope.run_block()
+                scope_traces = scope.get_block_traces()
+                time_now = datetime.datetime.today()
+                mot_power_dict = {}
+                for chl_idx, params in self.channel_dict.items():
+                    label = params['channel_label']
+                    trace = scope_traces[chl_idx]
+                    mot_power_dict.update({f'{label}mean_in_mv': np.mean(trace),
+                                        f'{label}std_in_mv': np.std(trace)})
+
+                self.append_to_backlog(mot_power_dict, time_now=time_now)
+                self.upload_to_breadboard()
+                time.sleep(self.refresh_time)
+                print('\n\n')
+
+
+if __name__ == '__main__':
+    mot_power_monitor = MOTPowerMonitor(refresh_time=0)
+    mot_power_monitor.main()
